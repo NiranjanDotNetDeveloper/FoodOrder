d0132ed8a2dbd254510e62cf5a641b4ad997f609
Niranjan <iniranjankumarsingh.gmail.com>
2025-04-17 20:25:30 +0530

    Add project files.

---
+++ FoodOrder/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrder.Controllers
+{
+    [Route("[controller]/[action]")]
+    [Route("/")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
